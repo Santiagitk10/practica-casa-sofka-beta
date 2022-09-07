b3e7316db16e651808d78e876f58359cd622c2fb
Santiago Sierra <santiagosierrak10@gmail.com>
2022-09-07 18:36:47 -0500

    GetAllPosts and GetPostById implemented

--- src/main/java/com/posada/santiago/betapostsandcomments/APPRENTICESbetapostscomments/business/usecases/BringPostById.java
+++ src/main/java/com/posada/santiago/betapostsandcomments/APPRENTICESbetapostscomments/business/usecases/BringPostById.java
@@ -3,9 +3,30 @@ package com.posada.santiago.betapostsandcomments.APPRENTICESbetapostscomments.bu
 
 
 
+import com.posada.santiago.betapostsandcomments.APPRENTICESbetapostscomments.application.adapters.repository.MongoViewRepository;
+import com.posada.santiago.betapostsandcomments.APPRENTICESbetapostscomments.business.gateways.model.PostViewModel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
 
 @Service
-public class BringPostById {
+@RequiredArgsConstructor
+public class BringPostById implements Function<String, Mono<PostViewModel>> {
+
+
     // finish the implementation of this class using the functional interfaces
+
+
+    public final MongoViewRepository repository;
+
+    @Override
+    public Mono<PostViewModel> apply(String postId) {
+        return this.repository.findByAggregateId(postId);
+    }
+
+
+
+
 }
